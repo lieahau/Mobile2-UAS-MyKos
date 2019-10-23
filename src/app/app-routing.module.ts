b5e17b833a0aa6fbd5a078d0192c6f69d8240fa4
Starfall Production <starfallproduction42@gmail.com>
2019-10-23 08:58:22 +0700

    Add pages and sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardPageModule' },
+  { path: 'overview', loadChildren: './overview/overview.module#OverviewPageModule' },
+  { path: 'room:id', loadChildren: './room/room.module#RoomPageModule' },
 ];
 
 @NgModule({
