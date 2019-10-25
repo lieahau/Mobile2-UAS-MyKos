a6493ac90554e71bf8a37ee0ad2a16125f643d12
Starfall Production <starfallproduction42@gmail.com>
2019-10-26 00:32:32 +0700

    implement modal for settings

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +7,109 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
+  numberOfRooms = 0;
+  roomIdValue: 'numeric' | 'alphabetic' = 'numeric';
+  maxDueDate = 30;
 
-  constructor() { }
+  constructor(
+    private alertController: AlertController
+  ) { }
 
   ngOnInit() {
   }
 
+  async setNumberOfRooms() {
+    const alert = await this.alertController.create({
+      header: 'Number of rooms',
+      inputs: [
+        {
+          name: 'numberOfRooms',
+          placeholder: 'How many room?',
+          type: 'number',
+          value: this.numberOfRooms
+        }
+      ],
+      buttons: [
+        {
+          role: 'cancel',
+          text: 'Cancel'
+        },
+        {
+          text: 'OK',
+          handler: (value) => {
+            console.log(value);
+            this.numberOfRooms = Math.round(value.numberOfRooms);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async setRoomIdValue() {
+    const alert = await this.alertController.create({
+      header: 'Room ID value',
+      inputs: [
+        {
+          name: 'numeric',
+          type: 'radio',
+          label: 'Numeric',
+          checked: this.roomIdValue === 'numeric',
+          value: 'numeric'
+        },
+        {
+          name: 'alphabetic',
+          type: 'radio',
+          label: 'Alphabetic',
+          checked: this.roomIdValue === 'alphabetic',
+          value: 'alphabetic'
+        }
+      ],
+      buttons: [
+        {
+          role: 'cancel',
+          text: 'Cancel'
+        },
+        {
+          text: 'OK',
+          handler: (value) => {
+            console.log(value);
+            this.roomIdValue = value;
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async setMaxDueDate() {
+    const alert = await this.alertController.create({
+      header: 'Maximal due date',
+      inputs: [
+        {
+          name: 'maxDueDate',
+          placeholder: 'How many day?',
+          type: 'number',
+          value: this.maxDueDate
+        }
+      ],
+      buttons: [
+        {
+          role: 'cancel',
+          text: 'Cancel'
+        },
+        {
+          text: 'OK',
+          handler: (value) => {
+            console.log(value);
+            this.maxDueDate = Math.round(value.maxDueDate);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
